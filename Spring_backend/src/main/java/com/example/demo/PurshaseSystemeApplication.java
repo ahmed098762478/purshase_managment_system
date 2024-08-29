c11c886c79734ac2c431fca7007d454c1a5b500a
ahmed098762478 <162605372+ahmed098762478@users.noreply.github.com>
2024-08-29 12:56:24 +0100

    creation du projet spring

---
+++ Spring_backend/src/main/java/com/example/demo/PurshaseSystemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurshaseSystemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurshaseSystemeApplication.class, args);
+	}
+
+}
