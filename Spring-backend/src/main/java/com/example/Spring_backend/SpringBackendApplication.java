59c57ecbfd7904e447f77de92259032cae61553a
ahmed098762478 <162605372+ahmed098762478@users.noreply.github.com>
2024-08-29 14:41:05 +0100

    commit

---
+++ Spring-backend/src/main/java/com/example/Spring_backend/SpringBackendApplication.java
@@ -0,0 +1,16 @@
+package com.example.Spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class SpringBackendApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(SpringBackendApplication.class, args);
+	}
+
+}
