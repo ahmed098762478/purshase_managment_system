792a3781385bf42e67823ce6165424576a91efcf
ahmed098762478 <162605372+ahmed098762478@users.noreply.github.com>
2024-08-31 11:43:10 +0100

    navigation de login vers page la  principale

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../css/login.css';
-import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate('/');  
+    };
+
     return (
         <div className="login-center">
-            <a href="index.html"> 
-                <img className="login-img" src="img/ocp.png" alt="OCP Logo" />
-            </a>
+            <img 
+                className="login-img" 
+                src="/img/ocp.png" 
+                alt="OCP Logo" 
+                onClick={handleLogoClick} // Appel de la fonction de redirection
+                style={{ cursor: 'pointer' }} // Changer le curseur pour indiquer qu'il est cliquable
+            />
             <h1>Login</h1>
             <form action="" method="post">
                 <div className="login-txt_field">
